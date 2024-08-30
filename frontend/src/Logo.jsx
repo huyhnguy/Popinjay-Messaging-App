3cd45056e6934408cee6d368e28649ebb31e5924
Huy Nguyen <huy.h.nguy@gmail.com>
2024-08-30 18:47:58 -0500

    change logo and site name

--- frontend/src/Logo.jsx
+++ frontend/src/Logo.jsx
@@ -1,7 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faDragon} from '@fortawesome/free-solid-svg-icons'
+
 export default function Logo() {
     return(
-        <>
-            <h1 className="site-name">TalkBox</h1>
-        </>
+        <div style={{display: "flex", gap: "1rem" }}>
+            <FontAwesomeIcon icon={faDragon} style={{height: "3rem"}}/>
+            <h1 className="site-name">Popinjay</h1>
+        </div>
     )
 }
