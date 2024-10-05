e6f684a24ad23013762d0da3698ff13bbad2492c
Huy Nguyen <huy.h.nguy@gmail.com>
2024-10-04 20:51:22 -0500

    refactor SignUp component to typescript

--- frontend/src/views/SignUp.tsx
+++ frontend/src/views/SignUp.tsx
@@ -2,18 +2,33 @@ import Logo from "../components/Logo"
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+type Errors = {
+    display_name?: {
+        msg: string
+    },
+    username?: {
+        msg: string
+    },
+    password?: {
+        msg: string
+    },
+    confirm_password?: {
+        msg: string
+    }
+} | null
+
 export default function SignUp() {
-    const [errors, setErrors] = useState(null);
+    const [errors, setErrors] = useState<Errors>(null);
 
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e: React.MouseEvent<HTMLInputElement>) => {
         e.preventDefault();
 
-        const username = document.getElementById('username').value;
-        const displayName = document.getElementById('displayname').value;
-        const password = document.getElementById('password').value;
-        const confirmPassword = document.getElementById('confirmpassword').value;
+        const username = (document.getElementById('username') as HTMLInputElement).value;
+        const displayName =(document.getElementById('displayname')as HTMLInputElement).value;
+        const password = (document.getElementById('password') as HTMLInputElement).value;
+        const confirmPassword = (document.getElementById('confirmpassword') as HTMLInputElement).value;
         
         const userInputs = {
             display_name: displayName,
@@ -33,10 +48,10 @@ export default function SignUp() {
           .then(res => res.json())
           .then(res => {
             if (res.errors) {
-                const displayNameErrors = res.errors.filter(error => error.path === "display_name");
-                const usernameErrors = res.errors.filter(error => error.path === "username");
-                const passwordErrors = res.errors.filter(error => error.path === "password");
-                const confirmPasswordErrors = res.errors.filter(error => error.path === "confirm_password");
+                const displayNameErrors = res.errors.filter((error: { path: string}) => error.path === "display_name");
+                const usernameErrors = res.errors.filter((error: { path: string}) => error.path === "username");
+                const passwordErrors = res.errors.filter((error: { path: string}) => error.path === "password");
+                const confirmPasswordErrors = res.errors.filter((error: { path: string}) => error.path === "confirm_password");
 
                 setErrors({
                     display_name: displayNameErrors[0],
@@ -58,28 +73,28 @@ export default function SignUp() {
                 <h2>Create An Account</h2>
                 <form action="" method="POST" >
                     <div className="input-containers">
-                        <input className="input" type="text" name="displayname" id="displayname" placeholder='Display Name' required style={{ borderColor: errors && errors.display_name && "red" }}/>
+                        <input className="input" type="text" name="displayname" id="displayname" placeholder='Display Name' required style={{ borderColor: errors && errors.display_name ? "red" : "black" }}/>
                         { errors && errors.display_name &&
                         <p className="error-message">{errors.display_name.msg}</p>
                         }
                     </div>
 
                     <div className="input-containers">
-                        <input className="input" type="text" name="username" id="username" placeholder='Username' required style={{ borderColor: errors && errors.username && "red" }}/>
+                        <input className="input" type="text" name="username" id="username" placeholder='Username' required style={{ borderColor: errors && errors.username ? "red" : "black" }}/>
                         { errors && errors.username &&
                         <p className="error-message">{errors.username.msg}</p>
                         }
                     </div>
 
                     <div className="input-containers">
-                    <input className="input" type="password" name="password" id="password" placeholder='Password' required style={{ borderColor: errors && errors.password && "red" }}/>
+                    <input className="input" type="password" name="password" id="password" placeholder='Password' required style={{ borderColor: errors && errors.password ? "red" : "black"}}/>
                     { errors && errors.password &&
                         <p className="error-message">{errors.password.msg}</p>
                     }
                     </div>
 
                     <div className="input-containers">
-                    <input className="input" type="password" name="confirmpassword" id="confirmpassword" placeholder='Confirm Password' required style={{ borderColor: errors && errors.confirm_password && "red" }}/>
+                    <input className="input" type="password" name="confirmpassword" id="confirmpassword" placeholder='Confirm Password' required style={{ borderColor: errors && errors.confirm_password ? "red" : "black" }}/>
                     { errors && errors.confirm_password &&
                         <p className="error-message">{errors.confirm_password.msg}</p>
                     }
