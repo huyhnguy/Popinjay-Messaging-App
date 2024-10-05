279ce9e4c073db95f92776e4df39cddab7c2e3a8
Huy Nguyen <huy.h.nguy@gmail.com>
2024-10-04 20:38:24 -0500

    refactor Settings component to typescript

--- frontend/src/views/Settings.tsx
+++ frontend/src/views/Settings.tsx
@@ -6,13 +6,25 @@ import { faCircleXmark, faLock } from '@fortawesome/free-solid-svg-icons'
 import ProfilePic from "../components/ProfilePic";
 import { useNavigate } from "react-router-dom";
 
+type Base64 = string | null
+type Errors = {
+    display_name?: {
+        msg: string
+    },
+    about_me?: {
+        msg: string
+    }
+} | null
+
+type AboutMe = string | undefined 
+
 export default function Settings() {
     const [displayName, setDisplayName] = useState(undefined);
-    const [base64Pic, setBase64Pic] = useState(null);
-    const [aboutMe, setAboutMe] = useState(null);
+    const [base64Pic, setBase64Pic] = useState<Base64>(null);
+    const [aboutMe, setAboutMe] = useState<AboutMe>(undefined);
     const [guest, setGuest] = useState(false);
     const [aboutMeRemainingCharacters, setAboutMeRemainingCharacters] = useState(150);
-    const [errors, setErrors] = useState(null);
+    const [errors, setErrors] = useState<Errors>(null);
 
     const navigate = useNavigate();
 
@@ -27,9 +39,7 @@ export default function Settings() {
           })
           .then(res => {
             if (res.ok) { return res.json() }
-            const error = new Error(res.message);
-            error.code = res.status;
-            throw error
+            throw Error
           })
           .then(res => {
             console.log(res.profile_picture);
@@ -48,49 +58,52 @@ export default function Settings() {
         })
     }, [])
 
-    const convertToBase64 = (file) => {
-        return new Promise((resolve, reject) => {
+    const convertToBase64 = (file: File) => {
+        const base64String = new Promise<string>((resolve, reject) => {
             const fileReader = new FileReader();
             fileReader.readAsDataURL(file); 
             fileReader.onload = () => {
-                resolve(fileReader.result)
+                if (typeof fileReader.result === 'string')
+                    resolve(fileReader.result)
             }
             fileReader.onerror = (error) => {
                 reject(error)
             }
         })
+        return base64String
     }
 
-    const handleFileUpload = async (e) => {
+    const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
         if (guest) {
             e.preventDefault();
             return
         }
-        const file = e.target.files[0];
+        const file = e.target.files![0];
         const base64 = await convertToBase64(file);
         setBase64Pic(base64);
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        const newDisplayName = document.getElementById("display-name").value;
-        const aboutMe = document.querySelector(".about-me-input").value;
+        const newDisplayName = (document.getElementById("display-name") as HTMLInputElement).value;
+        const aboutMe = (document.querySelector(".about-me-input") as HTMLInputElement).value;
 
         const formData = new FormData();
         formData.append("display_name", newDisplayName);
         formData.append("about_me", aboutMe);
 
-        if (!base64Pic && !document.getElementById("profile-picture").files[0]) {
+        const profilePictureInput = (document.getElementById("profile-picture") as HTMLInputElement).files
+
+        if (!base64Pic && !profilePictureInput![0]) {
             // no previous picture and no picturer uploaded
-            formData.append("profile_picture", null);
+            formData.append("profile_picture", null!);
             formData.append("picture_status", "delete")
-        } else if (base64Pic && document.getElementById("profile-picture").files[0]) { 
+        } else if (base64Pic && profilePictureInput![0]) { 
             //if the user doesn't have a picture yet and they upload a picture
-            const profilePic = document.getElementById("profile-picture").files[0];
-            formData.append("profile_picture", profilePic);
-        } else if (base64Pic && !document.getElementById("profile-picture").files[0]) {
+            formData.append("profile_picture", profilePictureInput![0]);
+        } else if (base64Pic && !profilePictureInput![0]) {
             //if the user does have a picture and they dont upload any picture, dont do anything
-            formData.append("profile_picture", null);
+            formData.append("profile_picture", null!);
         }
 
         fetch('/api/users/settings', {
@@ -104,8 +117,8 @@ export default function Settings() {
           .then(res => res.json())
           .then(res => {
             if (res.errors) {
-                const displayNameErrors = res.errors.filter(error => error.path === "display_name");
-                const aboutMeErrors = res.errors.filter(error => error.path === "about_me");
+                const displayNameErrors = res.errors.filter((error: { path: string }) => error.path === "display_name");
+                const aboutMeErrors = res.errors.filter((error: { path: string}) => error.path === "about_me");
                 setErrors({
                     display_name: displayNameErrors[0],
                     about_me: aboutMeErrors[0]
@@ -122,7 +135,7 @@ export default function Settings() {
 
     }
 
-    const handleLogOut = (e) => {
+    const handleLogOut = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
 
         fetch('/api/logout', {
@@ -136,27 +149,28 @@ export default function Settings() {
             if (res.ok) {
                 navigate('/');
             }
-            const error = new Error(res.message);
-            error.code = res.status;
-            throw error
+            throw Error
         }).catch(err => {
             console.log(err);
         })
     }
 
-    const countRemainingCharacters = (e) => {
+    const countRemainingCharacters = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         const text = e.target.value;
         const characters = text.length;
         const remainingCharacters = 150 - characters;
         const remainingCharactersText = document.querySelector('.about-me-remaining-characters');
-        if (remainingCharacters >= 0) {
-            remainingCharactersText.classList.add("green")
-            remainingCharactersText.classList.remove("red")
-        } else {
-            remainingCharactersText.classList.add("red")
-            remainingCharactersText.classList.remove("green")
+        if (remainingCharactersText) {
+            if (remainingCharacters >= 0) {
+                remainingCharactersText.classList.add("green")
+                remainingCharactersText.classList.remove("red")
+            } else {
+                remainingCharactersText.classList.add("red")
+                remainingCharactersText.classList.remove("green")
+            }
         }
         setAboutMeRemainingCharacters(remainingCharacters);
+
         return
     }
 
@@ -184,7 +198,7 @@ export default function Settings() {
                                         </button>
                                     </>
                                     :
-                                    <label htmlFor="profile-picture" style={{ cursor: !guest && "pointer" }}>
+                                    <label htmlFor="profile-picture" style={{ cursor: !guest ? "pointer" : "default" }}>
                                         <ProfilePic size="10rem"/>
                                     </label>
                                 }
@@ -197,7 +211,7 @@ export default function Settings() {
                                 }
                                 Profile Picture 
                             </label>
-                            <input style={{ cursor: !guest && "pointer", color: guest && "grey" }} className="input" type="file" id="profile-picture" name="profile-picture" accept="image/*" defaultValue={ base64Pic && base64Pic } onChange={(e) => {handleFileUpload(e)}} disabled={guest ? true : false}/>
+                            <input style={{ cursor: !guest ? "pointer" : "default", color: guest ? "grey" : "black" }} className="input" type="file" id="profile-picture" name="profile-picture" accept="image/*" defaultValue={ base64Pic ? base64Pic : undefined } onChange={(e) => {handleFileUpload(e)}} disabled={guest ? true : false}/>
                         </div>
                         <div className="form-section" style={{ alignItems: "start" }}>
                             <label htmlFor="display-name">
@@ -207,7 +221,7 @@ export default function Settings() {
                                 Display Name
                             </label>
                             <div className="input-containers">
-                                <input className="input" id="display-name" type="text" defaultValue={displayName} disabled={guest ? true : false} style={{ color: guest && "grey", borderColor: errors && errors.display_name && "red" }}/>
+                                <input className="input" id="display-name" type="text" defaultValue={displayName} disabled={guest ? true : false} style={{ color: guest ? "grey" : "black", borderColor: errors && errors.display_name ? "red" : "black" }}/>
                                 { errors && errors.display_name &&
                                     <p className="error-message">{errors.display_name.msg}</p>
                                 }
@@ -222,7 +236,7 @@ export default function Settings() {
                                 About Me
                             </label>
                             <div style={{position: "relative", width: "100%"}}>
-                                <textarea className="about-me-input" name="about-me" id="about-me" rows="4" disabled={guest ? true : false} style={{ color: guest && "grey", borderColor: errors && errors.about_me && "red" }} onChange={(e) => {countRemainingCharacters(e)}} defaultValue={aboutMe}></textarea>
+                                <textarea className="about-me-input" name="about-me" id="about-me" rows={4} disabled={guest ? true : false} style={{ color: guest ? "grey" : "black", borderColor: errors && errors.about_me ? "red" : "black" }} onChange={(e) => {countRemainingCharacters(e)}} defaultValue={aboutMe}></textarea>
                                 <p className='about-me-remaining-characters green'>{aboutMeRemainingCharacters}</p>
                             
                                 { errors && errors.about_me &&
@@ -231,7 +245,7 @@ export default function Settings() {
                             </div>
 
                         </div>
-                        <button className="submit" style={{ backgroundColor: guest && "grey", pointerEvents: guest && "none" }}onClick={handleSubmit}>Save</button>
+                        <button className="submit" style={{ backgroundColor: guest ? "grey" : "#007BFF", pointerEvents: guest ? "none" : "auto" }}onClick={handleSubmit}>Save</button>
                         <button className="submit" style={{backgroundColor: "red"}} onClick={handleLogOut}>Log out</button>
                     </form>
                 </div>
